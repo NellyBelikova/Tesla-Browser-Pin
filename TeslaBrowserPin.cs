8ef6abdba887a80de9fc901d5d5bb9ca82035977
beeox <beeox@DESKTOP-5JOJ0V4>
2022-05-17 08:31:32 +0300

    Добавьте файлы проекта.

---
+++ TeslaBrowserPin.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+
+namespace TeslaPin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.teslabrowser.com/";
+            NavigateToTesla(url);
+        }
+
+        public static void NavigateToTesla(string url)
+        {
+            string pathTesla = @"C:\Program Files\TeslaBrowser\TeslaBrowser.exe";
+            try
+            {
+                if (File.Exists(pathTesla)) Process.Start(pathTesla, url);
+                else Process.Start(url);
+            }
+            catch
+            {
+                Process.Start(Path.Combine("microsoft-edge:", url));
+            }
+        }
+
+    }
+}
